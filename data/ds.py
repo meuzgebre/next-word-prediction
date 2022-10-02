85e5f7a41332c0ffecd8a4995578d74edbce50e9
Meuz Kidane <95613515+meuzgebre@users.noreply.github.com>
2022-10-02 06:44:14 -0800

    Delete ds.py

--- data/ds.py
+++
@@ -1,6 +0,0 @@
-import os
-amh = open('amh.txt', encoding='utf-8').read().strip().split('\n')
-eng = open('eng.txt', encoding='utf-8').read().strip().split('\n')
-pairs = []
-for idx in range(len(eng)):
-  open('datasets.txt', encoding='utf-8').write(f'{amh[idx]}\t{eng[idx]}')
